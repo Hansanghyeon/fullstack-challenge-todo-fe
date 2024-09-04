8a7caa99742c1ed666e7ca0d1c9336f36a9a1828
Hansanghyeon <999@hyeon.pro>
2024-09-04 12:39:33 +0000

    feat: 토큰검증 로직 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { useMutation } from '@tanstack/react-query'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { StrictMode }                   from 'react'
-import ReactDOM                         from 'react-dom/client'
+import React from 'react'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 import './style.css'
 
 // Import the generated route tree
+import { getAuthenticate } from './api/auth/authenticate'
 import { QueryClientProvider, queryClient } from './app/provider/tanstack-query'
-import { routeTree }                        from './routeTree.gen'
-import { useAuth }                          from './shared/hooks/use-auth'
+import { routeTree } from './routeTree.gen'
+import { useAuth } from './shared/hooks/use-auth'
+
 
 // Create a new router instance
 const router = createRouter({
@@ -26,6 +30,21 @@ declare module '@tanstack/react-router' {
 
 function App() {
   const auth = useAuth()
+  const { mutate } = useMutation({
+    mutationFn: getAuthenticate(),
+    onError() {
+      auth.signout()
+    }
+  })
+
+  
+  // 앱이 실행될때 딱한번 진행한다.
+  // 로그인이 되어있다면 해당 토큰이 실제로 인증처리가 가능한지 확인한다.
+  React.useEffect(() => {
+    if (auth.value) {
+      mutate({})
+    } 
+  }, [])
   return <RouterProvider router={router} context={{ auth }} />
 }
 
